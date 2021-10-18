d0aca683858ea924266f2a77660dd12255dd2c9f
Franck Seigneuret <f.seigneuret@mazarine.com>
2021-10-18 17:30:48 +0200

    chore: add and setup commitlint

--- git-precommit-checks.config.js
+++ git-precommit-checks.config.js
@@ -4,17 +4,22 @@ module.exports = {
       filter: /\.js$/,
       nonBlocking: "true",
       message: "🤫 Oula, aurais-tu oublié des `console.log` inopportuns ?",
-      regex: "console\\.log",
+      regex: /^\s*console\.log/,
     },
     {
       // ici, le blocage du commit est apppliqué sur tous les fichiers (filter n'est pas défini)
       message: "😨 On dirait que tu as oublié des marqueurs de conflits",
-      regex: "/^[<>|=]{4,}/m",
+      regex: /^[<>|=]{4,}/m,
     },
     {
       message: "🤔 Aurais-tu oublié de finir des développement ?",
       nonBlocking: "true",
-      regex: "(?:FIXME|TODO)",
+      regex: /(?:FIXME|TODO)/,
+    },
+    {
+      message:
+        'Arrêt du commit : tu as renseigné des choses qui ne doivent pas être commitées !',
+      regex: /do not commit/i,
     },
   ],
 };
